4d725116fcf4fc7acbb01bc8f535fe8dbbfbaaad
gomiles <56161454+gomiles@users.noreply.github.com>
2022-03-10 19:32:17 +0100

    reformat

--- starter_files/run_pipeline.py
+++ starter_files/run_pipeline.py
@@ -1,9 +1,9 @@
 import os
 
+import requests
+from azureml.core import Workspace
 from azureml.core.authentication import InteractiveLoginAuthentication
 from azureml.pipeline.core import PublishedPipeline
-from azureml.core import Workspace
-import requests
 
 interactive_auth = InteractiveLoginAuthentication()
 auth_header = interactive_auth.get_authentication_header()
@@ -12,11 +12,13 @@ subscription_id = os.getenv("SUBSCRIPTIONID")
 resource_group = os.getenv("RESOURCEGROUP")
 workspace_name = os.getenv("WORKSPACENAME")
 
-ws = Workspace(subscription_id=subscription_id, resource_group=resource_group, workspace_name=workspace_name, auth=interactive_auth)
+ws = Workspace(subscription_id=subscription_id, resource_group=resource_group, workspace_name=workspace_name,
+               auth=interactive_auth)
 
 published_pipeline = PublishedPipeline.get(ws, 'f43f5605-9636-4cd3-9f75-7f97053224db')
 pipeline_endpoint = published_pipeline.endpoint
 
-response = requests.post(pipeline_endpoint, headers=auth_header, json={"ExperimentName": "udacity-automlstep-classification"})
+response = requests.post(pipeline_endpoint, headers=auth_header,
+                         json={"ExperimentName": "udacity-automlstep-classification"})
 print(response)
 print(response.json())
